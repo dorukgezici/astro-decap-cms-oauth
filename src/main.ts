bddb8528e0127185cc489eb2fbeb5f009ef42a35
Doruk Gezici <doruk@gezici.me>
2023-10-08 01:30:52 +0200

    Added some configuration options for the integration

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,45 @@
 import type { AstroIntegration } from "astro";
 
-export type DecapCMSOptions = {};
+export type DecapCMSOptions = {
+  adminRoute?: string;
+  oauthDisabled?: boolean;
+  oauthLoginRoute?: string;
+  oauthCallbackRoute?: string;
+};
 
-export default function decapCMS(options: DecapCMSOptions = {}): AstroIntegration {
+export default function decapCMS({
+  adminRoute = "/admin",
+  oauthDisabled = false,
+  oauthLoginRoute = "/oauth",
+  oauthCallbackRoute = "/oauth/callback",
+}: DecapCMSOptions): AstroIntegration {
+  if (!adminRoute.startsWith("/") || !oauthLoginRoute.startsWith("/") || !oauthCallbackRoute.startsWith("/")) {
+    throw new Error('`adminRoute`, `oauthLoginRoute` and `oauthCallbackRoute` options must start with "/"');
+  }
+
   return {
     name: "astro-decap-cms-oauth",
     hooks: {
       "astro:config:setup": async ({ injectRoute }) => {
+        // mount DecapCMS admin dashboard
         injectRoute({
-          pattern: "/admin",
+          pattern: adminRoute,
           entryPoint: "astro-decap-cms-oauth/src/admin.astro",
         });
 
-        injectRoute({
-          pattern: "/oauth",
-          entryPoint: "astro-decap-cms-oauth/src/oauth/index.ts",
-        });
+        if (!oauthDisabled) {
+          // OAuth backend - sign in route
+          injectRoute({
+            pattern: oauthLoginRoute,
+            entryPoint: "astro-decap-cms-oauth/src/oauth/index.ts",
+          });
 
-        injectRoute({
-          pattern: "/oauth/callback",
-          entryPoint: "astro-decap-cms-oauth/src/oauth/callback.ts",
-        });
+          // OAuth backend - callback route
+          injectRoute({
+            pattern: oauthCallbackRoute,
+            entryPoint: "astro-decap-cms-oauth/src/oauth/callback.ts",
+          });
+        }
       },
     },
   };
