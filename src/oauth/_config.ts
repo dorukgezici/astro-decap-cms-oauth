39e88c56d52c9dda6b5708b2e668e5357ef3bdd5
Doruk Gezici <doruk@gezici.me>
2024-06-23 22:59:49 +0200

    Using import.meta.env instead of process.env #16

--- src/oauth/_config.ts
+++ src/oauth/_config.ts
@@ -1,5 +1,5 @@
-export const clientId = process.env.OAUTH_GITHUB_CLIENT_ID;
-export const clientSecret = process.env.OAUTH_GITHUB_CLIENT_SECRET;
+export const clientId = import.meta.env.OAUTH_GITHUB_CLIENT_ID;
+export const clientSecret = import.meta.env.OAUTH_GITHUB_CLIENT_SECRET;
 
 export const authUrl = `https://github.com/login/oauth/authorize?client_id=${clientId}&scope=repo,user`;
 export const tokenUrl = "https://github.com/login/oauth/access_token";
